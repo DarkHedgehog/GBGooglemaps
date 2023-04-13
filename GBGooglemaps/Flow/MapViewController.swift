a873976122b97b83d7f495fc204cc8d5579dc955
Derevenskikh Aleksander <hedgehog@kartis.ru>
2023-04-13 15:08:34 +0300

    add start\stop tracking buttons

--- GBGooglemaps/Flow/MapViewController.swift
+++ GBGooglemaps/Flow/MapViewController.swift
@@ -6,20 +6,34 @@
 //
 
 import UIKit
+import CoreData
 import GoogleMaps
 
 class MapViewController: UIViewController {
     @IBOutlet weak var mapView: GMSMapView!
+    @IBOutlet weak var trackActivityButton: UIButton!
 
+    var route: GMSPolyline?
+    var routePath: GMSMutablePath?
     var locationManager: CLLocationManager?
+    var isTrackingActive = false
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        configureUI()
         configureMap()
         configureLocationManager()
     }
 
+    private func configureUI() {
+        updateTrackActivityButton()
+    }
+
+    private func updateTrackActivityButton() {
+        let title = isTrackingActive ? "Stop tracking" : "Start tracking"
+        trackActivityButton.setTitle(title, for: .normal)
+    }
 
     private func configureMap() {
         mapView.isMyLocationEnabled = true
@@ -28,7 +42,11 @@ class MapViewController: UIViewController {
     private func configureLocationManager() {
         locationManager = CLLocationManager()
         locationManager?.delegate = self
-        locationManager?.requestWhenInUseAuthorization()
+        locationManager?.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
+        locationManager?.allowsBackgroundLocationUpdates = true
+        locationManager?.pausesLocationUpdatesAutomatically = false
+        locationManager?.startMonitoringSignificantLocationChanges()
+        locationManager?.requestAlwaysAuthorization()
     }
 
     @IBAction func goToMoskow(_ sender: Any) {
@@ -37,9 +55,20 @@ class MapViewController: UIViewController {
         mapView.animate(to: camera)
     }
 
-    @IBAction func makeMarker(_ sender: Any) {
-        locationManager?.requestLocation()
-        locationManager?.startUpdatingLocation()
+    @IBAction func toggleTracking(_ sender: Any) {
+        isTrackingActive.toggle()
+        updateTrackActivityButton()
+
+        if isTrackingActive {
+            route?.map = nil
+            route = GMSPolyline()
+            routePath = GMSMutablePath()
+            route?.map = mapView
+            locationManager?.startUpdatingLocation()
+        } else {
+            locationManager?.stopUpdatingLocation()
+        }
+
     }
 }
 
@@ -48,12 +77,13 @@ extension MapViewController: CLLocationManagerDelegate {
         guard let coordinate = locations.last?.coordinate else {
             return
         }
+//        let marker = GMSMarker(position: coordinate)
+//        marker.map = mapView
+        routePath?.add(coordinate)
+        route?.path = routePath
+
         let camera = GMSCameraPosition.camera(withTarget: coordinate, zoom: 17)
         mapView.animate(to: camera)
-        let marker = GMSMarker(position: coordinate)
-        marker.map = mapView
-
-        print(locations)
     }
 
     func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
